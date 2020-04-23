9196707723803217e95107348704e0435a324645
Anatoli Beliaev <anatolib@microsoft.com>
2020-04-22 23:23:19 -0700

    Add OrchestrationFailureException

---
+++ src/Durable/OrchestrationFailureException.cs
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+namespace Microsoft.Azure.Functions.PowerShellWorker.Durable
+{
+    using System;
+
+    internal class OrchestrationFailureException : Exception
+    {
+        public OrchestrationFailureException()
+        {
+        }
+
+        public OrchestrationFailureException(string message) : base(message)
+        {
+        }
+
+        public OrchestrationFailureException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
