2173398916a75538f7e35c33e765ebe7dea21b6e
Anatoli Beliaev <anatolib@microsoft.com>
2020-04-24 00:09:14 -0700

    Warnings before and after invoking activities

--- examples/durable/DurableApp/FunctionChainingOrchestrator/run.ps1
+++ examples/durable/DurableApp/FunctionChainingOrchestrator/run.ps1
@@ -6,9 +6,17 @@ Write-Host 'FunctionChainingOrchestrator: started.'
 
 $output = @()
 
+Write-Warning 'Invoking SayHello(Tokyo)'
 $output += Invoke-ActivityFunction -FunctionName 'SayHello' -Input 'Tokyo'
+Write-Warning 'Invoked SayHello(Tokyo)'
+
+Write-Warning 'Invoking SayHello(Seattle)'
 $output += Invoke-ActivityFunction -FunctionName 'SayHello' -Input 'Seattle'
+Write-Warning 'Invoked SayHello(Seattle)'
+
+Write-Warning 'Invoking SayHello(London)'
 $output += Invoke-ActivityFunction -FunctionName 'SayHello' -Input 'London'
+Write-Warning 'Invoked SayHello(London)'
 
 Write-Host 'FunctionChainingOrchestrator: finished.'
 
