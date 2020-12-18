087084b4d7e55e9fceb0478f138f85b4a2f81e03
Anatoli Beliaev <anatolib@microsoft.com>
2020-12-17 18:14:46 -0800

    TEMP: run E2E durable tests locally

--- test/E2E/Start-E2ETest.ps1
+++ test/E2E/Start-E2ETest.ps1
@@ -25,35 +25,7 @@ if ($IsWindows) {
     }
 }
 
-$coreToolsDownloadURL = $null
-if ($UseCoreToolsBuildFromIntegrationTests.IsPresent)
-{
-    $coreToolsDownloadURL = "https://funcintegrationtests.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest/Azure.Functions.Cli.$os-$arch.zip"
-    $env:CORE_TOOLS_URL = "https://funcintegrationtests.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest"
-}
-else
-{
-    $coreToolsDownloadURL = "https://functionsclibuilds.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest/Azure.Functions.Cli.$os-$arch.zip"
-    if (-not $env:CORE_TOOLS_URL)
-    {
-        $env:CORE_TOOLS_URL = "https://functionsclibuilds.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest"
-    }
-}
-
 $FUNC_CLI_DIRECTORY = Join-Path $PSScriptRoot 'Azure.Functions.Cli'
-
-Write-Host 'Deleting Functions Core Tools if exists...'
-Remove-Item -Force "$FUNC_CLI_DIRECTORY.zip" -ErrorAction Ignore
-Remove-Item -Recurse -Force $FUNC_CLI_DIRECTORY -ErrorAction Ignore
-
-$version = Invoke-RestMethod -Uri "$env:CORE_TOOLS_URL/version.txt"
-Write-Host "Downloading Functions Core Tools (Version: $version)..."
-
-$output = "$FUNC_CLI_DIRECTORY.zip"
-Invoke-RestMethod -Uri $coreToolsDownloadURL -OutFile $output
-
-Write-Host 'Extracting Functions Core Tools...'
-Expand-Archive $output -DestinationPath $FUNC_CLI_DIRECTORY
 
 if (-not $UseCoreToolsBuildFromIntegrationTests.IsPresent)
 {
