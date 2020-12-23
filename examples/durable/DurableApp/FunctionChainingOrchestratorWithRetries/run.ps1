6879baef9419a24e762f2143edaa62719d67876f
Anatoli Beliaev <anatolib@microsoft.com>
2020-12-23 12:21:37 -0800

    Add retrying example

---
+++ examples/durable/DurableApp/FunctionChainingOrchestratorWithRetries/run.ps1
@@ -0,0 +1,15 @@
+using namespace System.Net
+
+param($Context)
+
+$output = @()
+
+$retryOptions = New-DurableRetryOptions `
+                    -FirstRetryInterval (New-Timespan -Seconds 5) `
+                    -MaxNumberOfAttempts 3
+
+$output += Invoke-ActivityFunction -FunctionName 'FlakyActivity' -Input 'Tokyo' -RetryOptions $retryOptions
+$output += Invoke-ActivityFunction -FunctionName 'FlakyActivity' -Input 'Seattle' -RetryOptions $retryOptions
+$output += Invoke-ActivityFunction -FunctionName 'FlakyActivity' -Input 'London' -RetryOptions $retryOptions
+
+$output
