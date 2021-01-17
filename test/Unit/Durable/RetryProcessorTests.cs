b7cb1182e12311cd2bcb4ddc446a5f0661152902
Anatoli Beliaev <anatolib@microsoft.com>
2021-01-16 22:46:29 -0800

    Add OutputsResultOnSuccess

--- test/Unit/Durable/RetryProcessorTests.cs
+++ test/Unit/Durable/RetryProcessorTests.cs
@@ -225,6 +225,40 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.Test.Durable
             Assert.Equal("failure reason 1", actualFailureReason);
         }
 
+        [Theory]
+        [InlineData(1, 1, false)]
+        [InlineData(1, 1, true)]
+        [InlineData(3, 1, false)]
+        [InlineData(3, 1, true)]
+        [InlineData(3, 2, false)]
+        [InlineData(3, 2, true)]
+        [InlineData(3, 3, false)]
+        [InlineData(3, 3, true)]
+        [InlineData(100, 50, false)]
+        [InlineData(100, 50, true)]
+        [InlineData(100, 100, false)]
+        [InlineData(100, 100, true)]
+        public void OutputsResultOnSuccess(int maxNumberOfAttempts, int performedAttempts, bool replay)
+        {
+            const string SuccessOutput = "success output";
+            var history = CreateSuccessHistory(performedAttempts, SuccessOutput, replay);
+
+            object actualOutput = null;
+
+            var shouldRetry = RetryProcessor.Process(
+                history,
+                maxNumberOfAttempts,
+                output: obj =>
+                        {
+                            Assert.Null(actualOutput);
+                            actualOutput = obj;
+                        },
+                onFailure: reason => { Assert.True(false, $"Unexpected failure: {reason}"); });
+
+            Assert.False(shouldRetry);
+            Assert.Equal(SuccessOutput, actualOutput);
+        }
+
         private HistoryEvent[] CreateFailureHistory(
             int performedAttempts,
             Func<int, string> getFailureReason,
@@ -243,32 +277,6 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.Test.Durable
             }
 
             return result;
-        }
-
-        private HistoryEvent[] CreateSingleSuccessHistory(string output)
-        {
-            var taskScheduledEventId = GetUniqueEventId();
-
-            var history =
-                new List<HistoryEvent>
-                    {
-                        new HistoryEvent
-                            {
-                                EventType = HistoryEventType.TaskScheduled,
-                                EventId = taskScheduledEventId,
-                                IsProcessed = false
-                            },
-                        new HistoryEvent
-                            {
-                                EventType = HistoryEventType.TaskCompleted,
-                                EventId = -1,
-                                TaskScheduledId = taskScheduledEventId,
-                                Result = output,
-                                IsProcessed = false
-                            }
-                    };
-
-            return history.ToArray();
         }
 
         private HistoryEvent[] CreateSingleFailureHistory(bool includeTimerEvents, string failureReason)
@@ -314,6 +322,53 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.Test.Durable
                             IsProcessed = false
                         });
             }
+
+            return history.ToArray();
+        }
+
+        private HistoryEvent[] CreateSuccessHistory(
+            int performedAttempts,
+            string successOutput,
+            bool replay)
+        {
+            var result = new HistoryEvent[0];
+
+            for (var attempt = 1; attempt <= performedAttempts; ++attempt)
+            {
+                bool isLastAttempt = attempt == performedAttempts;
+
+                var next = isLastAttempt
+                                ? CreateSingleSuccessHistory(successOutput)
+                                : CreateSingleFailureHistory(includeTimerEvents: true, "dummy failure reason");
+
+                result = DurableTestUtilities.MergeHistories(result, next);
+            }
+
+            return result;
+        }
+
+        private HistoryEvent[] CreateSingleSuccessHistory(string output)
+        {
+            var taskScheduledEventId = GetUniqueEventId();
+
+            var history =
+                new List<HistoryEvent>
+                    {
+                        new HistoryEvent
+                            {
+                                EventType = HistoryEventType.TaskScheduled,
+                                EventId = taskScheduledEventId,
+                                IsProcessed = false
+                            },
+                        new HistoryEvent
+                            {
+                                EventType = HistoryEventType.TaskCompleted,
+                                EventId = -1,
+                                TaskScheduledId = taskScheduledEventId,
+                                Result = output,
+                                IsProcessed = false
+                            }
+                    };
 
             return history.ToArray();
         }
