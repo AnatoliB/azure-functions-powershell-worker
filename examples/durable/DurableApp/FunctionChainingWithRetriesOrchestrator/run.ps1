2f4705867345e14e7c1d522119d572b5d5b3bb05
Anatoli Beliaev <anatolib@microsoft.com>
2021-01-24 22:01:54 -0800

    Enable up to 7 retries in the example

--- examples/durable/DurableApp/FunctionChainingWithRetriesOrchestrator/run.ps1
+++ examples/durable/DurableApp/FunctionChainingWithRetriesOrchestrator/run.ps1
@@ -7,8 +7,8 @@ $ErrorActionPreference = 'Stop'
 $output = @()
 
 $retryOptions = New-DurableRetryOptions `
-                    -FirstRetryInterval (New-Timespan -Seconds 5) `
-                    -MaxNumberOfAttempts 3
+                    -FirstRetryInterval (New-Timespan -Seconds 1) `
+                    -MaxNumberOfAttempts 7
 
 $output += Invoke-ActivityFunction -FunctionName 'FlakyActivity' -Input 'Tokyo' -RetryOptions $retryOptions
 $output += Invoke-ActivityFunction -FunctionName 'FlakyActivity' -Input 'Seattle' -RetryOptions $retryOptions
